ff5211e72c7612ddec9c1267295ec82380fac1f2
cb3065 <christopherb3065@gmail.com>
2016-03-02 00:00:55 +0000

    Creating Schemas With Mongoose revised

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,29 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+
+/* a GET route for retrieving posts */
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+router.get('/posts', function(req, res, next) {
+	Post.find(function(err, posts){
+		if(err) { return next(err); }
+		res.json(posts);
+	});
+});
+
+
+/* a POST route for creating posts */
+router.post('/posts', function(req, res, next){
+	var post = new Post(req.body);
+	
+	post.save(function(err, post){
+		if(err) { return next(err); }
+		res.json(post);
+	});
+});
+
